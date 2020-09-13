50a21f240f791ff700353f98e664056d5b143d93
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-09-13 17:17:13 -0400

    Start header styling

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,3 +1,44 @@
+import AppBar from "@material-ui/core/AppBar"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import { makeStyles } from "@material-ui/core/styles"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import MenuIcon from "@material-ui/icons/Menu"
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}))
+
 export default function Header() {
-	return <div>Header</div>
+	const classes = useStyles()
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="fixed">
+				<Toolbar>
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="menu"
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						News
+					</Typography>
+					<Button color="inherit">Login</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	)
 }
